90e4ca1f8886032811a9bc81718679f5f33439d1
“Calixto” <l19070031@huetamo.tecnm.mx>
2026-01-04 21:21:12 -0600

    Organizando tags y agregando propiedades

--- js/index.js
+++ js/index.js
@@ -1,48 +1,48 @@
 
 // questions and responses
 import { responses } from "../data/response.js";
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
-// container of response
-let container = document.getElementById('chatbot');
-let buttonSend = document.getElementById('buttonSend');
-let textarea = document.getElementById('userInput');
-buttonSend.addEventListener('click', send);
+	// container of response
+	let container = document.getElementById('chatbot');
+	let buttonSend = document.getElementById('buttonSend');
+	let textarea = document.getElementById('userInput');
+	buttonSend.addEventListener('click', send);
 
-function checkInput() {
-	// check value input and function 
-	textarea.addEventListener('input', () => {
-		buttonSend.disabled = textarea.value.trim() === '';
+	function checkInput() {
+		// check value input and function 
+		textarea.addEventListener('input', () => {
+			buttonSend.disabled = textarea.value.trim() === '';
 
-	});
-}
-
-checkInput();
-
-function chat(input) {
-	// clear response 
-	container.innerHTML = '';
-	// input values minuscules 
-	input = input.toLowerCase();
-
-
-	for (const response of responses) {
-		// if input have a response, show response
-		if (input.includes(response.input)) {
-			return response.output;
-		}
+		});
 	}
-	return 'UPS! Lo siento, no comprendo lo que intentas decir';
-}
+
+	checkInput();
+
+	function chat(input) {
+		// clear response 
+		container.innerHTML = '';
+		// input values minuscules 
+		input = input.toLowerCase();
+		
+		
+		for (const response of responses) {
+			// if input have a response, show response
+			if (input.includes(response.input.toLowerCase())) {
+				return response.output;
+			}
+		}
+		return 'UPS! Lo siento, no comprendo lo que intentas decir';
+	}
 
 
 
-function send() {
+	function send() {
 
-	const userInput = document.getElementById('userInput').value;
-	const responseElement = document.createElement('p');
+		const userInput = document.getElementById('userInput').value;
+		const responseElement = document.createElement('p');
 
-	responseElement.innerHTML = `	
+		responseElement.innerHTML = `	
     <div class="chat-body">
         <div class="message incoming">
             <p>Tú: ${userInput} </p>
@@ -52,13 +52,13 @@ function send() {
         </div>
     </div>
 	`;
-	// add response to container
-	container.appendChild(responseElement);
-	// clear input
-	document.getElementById('userInput').value = '';
-	// add attribute disabled to input
-	buttonSend.setAttribute('disabled', true);
+		// add response to container
+		container.appendChild(responseElement);
+		// clear input
+		document.getElementById('userInput').value = '';
+		// add attribute disabled to input
+		buttonSend.setAttribute('disabled', true);
 
-}
+	}
 
 })
